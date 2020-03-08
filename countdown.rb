763b195b5e89445042da46a91cd851b7fdbf832b
Jose E Reyes <jreyes8631@gmail.com>
2020-03-08 13:32:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(second)
   return "HAPPY NEW YEAR!"
 
 end
+
+def countdown(second)
+  while second > 0
+    puts "#{second}SECOND(S) !"
+    second -= 1
+  end
+  return "HAPPY NEW YEAR!"
+
+end
