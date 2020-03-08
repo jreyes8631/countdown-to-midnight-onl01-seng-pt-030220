9f3aec69ef07725687c99ed8633d8e213848aad9
Jose E Reyes <jreyes8631@gmail.com>
2020-03-08 13:33:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,17 +4,16 @@ def countdown(second)
   while second > 0
     puts "#{second}SECOND(S) !"
     second -= 1
-  end
-  return "HAPPY NEW YEAR!"
+   end
+   return "HAPPY NEW YEAR!"
 
 end
 
 def countdown_with_sleep(second)
   while second > 0
     puts "#{second}SECOND(S) !"
-    second -= 1
     sleep(1)
-  end
-  return "HAPPY NEW YEAR!"
-
+    second -= 1
+   end
+   return "HAPPY NEW YEAR!"
 end
